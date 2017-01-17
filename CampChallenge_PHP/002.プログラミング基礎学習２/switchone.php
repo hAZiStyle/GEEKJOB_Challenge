c950177dd1058559a070a5f12db566fba135afa8
豊川　和樹 <hazistyle@me.com>
2017-01-17 16:51:39 +0900

    php課題提出「プログラミング基礎2」

---
+++ CampChallenge_PHP/002.プログラミング基礎学習２/switchone.php
@@ -0,0 +1,21 @@
+
+<?php
+
+$num = 1;
+$message = '';
+switch ($num) {
+  case 1 :
+  $message = 'one';
+    break;
+  case 2 :
+  $message = 'two';
+  default:
+  $message = '想定外';
+    break;
+}
+echo $message;
+
+
+
+
+ ?>
