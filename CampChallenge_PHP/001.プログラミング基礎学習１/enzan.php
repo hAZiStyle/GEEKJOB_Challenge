d0def950f8e1f9741cc2cb7027f17719c9f791bb
豊川　和樹 <hazistyle@me.com>
2017-01-17 13:31:56 +0900

    php課題提出「プログラミング基礎１」

---
+++ CampChallenge_PHP/001.プログラミング基礎学習１/enzan.php
@@ -0,0 +1,17 @@
+<?php
+
+const MSG = 5;
+$tasi = 1 + 1;
+$hiki = 10 - 8;
+$kake = 2 * 2;
+$wari = MSG / 5;
+
+echo $tasi .'<br>';
+echo $hiki .'<br>';
+echo $kake .'<br>';
+echo $wari .'<br>';
+
+
+
+
+ ?>
