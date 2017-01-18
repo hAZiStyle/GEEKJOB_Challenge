89360acbeaf3ab19f85bf4ca9c94d099384d5751
豊川　和樹 <hazistyle@me.com>
2017-01-18 16:14:31 +0900

    php課題提出「プログラミング基礎3」

---
+++ CampChallenge_PHP/003.プログラミング基礎学習３/function.php
@@ -0,0 +1,24 @@
+<?php
+
+function my_profile(){
+      echo "私の名前は豊川和樹です<br>";
+      echo "好きな音楽はロック<br>";
+      echo "趣味はゲームとネットサーフィン<br>";
+
+}
+my_profile();
+my_profile();
+my_profile();
+my_profile();
+my_profile();
+my_profile();
+my_profile();
+my_profile();
+my_profile();
+my_profile();
+
+
+
+
+
+ ?>
