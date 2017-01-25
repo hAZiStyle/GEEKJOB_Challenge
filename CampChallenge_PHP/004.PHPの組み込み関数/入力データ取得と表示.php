00e9eb87765b68ac2c62d1cad8b37fa016c4c262
豊川　和樹 <hazistyle@me.com>
2017-01-25 14:19:27 +0900

    php課題提出「データ操作」

---
+++ CampChallenge_PHP/004.PHPの組み込み関数/入力データ取得と表示.php
@@ -0,0 +1,24 @@
+
+
+<!--
+
+-->
+
+<html>
+  <head>
+  <title>自己紹介</title>
+ </head>
+ <body>
+
+
+  <form action="sample.php" method="post">
+  -名前-<br>
+  <input type="text" name="namae"><br>
+  -性別- <br>
+  <input type="radio" name="seibetu" value="男">男<br>
+  <input type="radio" name="seibetu" value="女">女<br>
+
+  -趣味-<br>
+  <input type="checkbox" name="syumi" value="ゲーム">ゲーム<br>
+  <input type="checkbox" name="syumi" value="読書">読書<br>
+  <input type="checkbox" name="syumi" value="スポーツ">スポーツ<br>
